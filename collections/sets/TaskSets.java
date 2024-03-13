128920a4a84b50b0ecabc22a4434e5435ceee22e
Zamiwka <zamalitem@gmail.com>
2024-03-13 20:52:40 +0100

    HashSettin uzundugu

--- collections/sets/TaskSets.java
+++ collections/sets/TaskSets.java
@@ -4,27 +4,37 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.TreeSet;
-import  java.util.Set;
+import java.util.Set;
 
 public class TaskSets {
     public static void main(String[] args) {
         List<String> name = new ArrayList<>();
         name.add("Asan");
         name.add("Yson");
-        name.add("Aman");
-        name.add("Alim");
-        name.add("Esen");
+        name.add("Seitek");
+        name.add("Semetei");
+        name.add("Sanirabiga");
+        name.add("Sanjar");
+        name.add("AnARBEK");
+        name.add("Esentai");
+        System.out.println("Spisok Imen:");
         System.out.println(name);
 
         HashSet<String> name1 = new HashSet<>(name);
-        System.out.println(name1);
+        System.out.println("HashSet (name1) with length > 5:");
+        for (String tamga : name1) {
+            if (tamga.length() > 5) {
+                System.out.println(tamga);
+            }
+        }
+         HashSet<String> name2 = new HashSet<>(name);
+         System.out.println("HashSettin uzundugu < 5:");
+         for (String tamga2 : name2) {
+         if  (tamga2.length() < 5) {
+                System.out.println(tamga2);
 
-        Set<String> name2 = new TreeSet<>(name);
-        System.out.println(name2);
 
-
-
-
+            }
+        }
     }
-
 }
