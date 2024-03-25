4e7bc3eb83cfac85bb8965b057837355bffbc682
Zamiwka <zamalitem@gmail.com>
2024-03-26 00:35:26 +0100

    oop kowuldu

---
+++ oop/Usluga.java
@@ -0,0 +1,38 @@
+package oop;
+
+public class Usluga {
+    public String name;
+    public String master;
+    public String color;
+    public float time;
+    public String design;
+    public String strazi;
+    public float bazovyiPrice;
+    public float dopPrice;
+    public String location;
+    public int number;
+
+    @Override
+    public String toString() {
+        return "Usluga{" +
+                "name='" + name + '\'' +
+                ", master='" + master + '\'' +
+                ", color='" + color + '\'' +
+                ", time=" + time +
+                ", design='" + design + '\'' +
+                ", strazi='" + strazi + '\'' +
+                ", bazovyiPrice=" + bazovyiPrice +
+                ", predPrice=" + dopPrice+
+                ", location='" + location + '\'' +
+                ", number=" + number +
+                '}';
+    }
+
+    public float obwayasena() {
+        return bazovyiPrice + dopPrice;
+
+    }
+
+
+    }
+
